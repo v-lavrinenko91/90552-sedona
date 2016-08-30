d321188a008b62519b59138e84e5bf470240ceb9
v-lavrinenko91 <vasya_1991_lav@list.ru>
2016-08-30 22:45:30 +0300

    Добиваем верстку Седону
    -js в отдельном файле
    -минимизация
    -автопрефиксы
    -кнопка отправить форму ссылкой
    и т.д.

---
+++ js/script-min.js
@@ -0,0 +1,1 @@
+var link = document.querySelector(".search-btn");var popup = document.querySelector(".search-form");var close = true;link.addEventListener("click", function(event) {event.preventDefault();if (close) {popup.classList.add("popup-down");close = false;}else {popup.classList.remove("popup-down");close = true;}});
