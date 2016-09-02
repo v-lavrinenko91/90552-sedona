ce776956a8e42c4701b2ce780c8d49df89658c1a
v-lavrinenko91 <vasya_1991_lav@list.ru>
2016-09-02 15:38:33 +0300

    Вторая попытка сдачи проекта)
    Исправлены все замечания по критериям

--- js/script-min.js
+++
@@ -1,1 +0,0 @@
-var link = document.querySelector(".search-btn");var popup = document.querySelector(".search-form");var close = true;link.addEventListener("click", function(event) {event.preventDefault();if (close) {popup.classList.add("popup-down");close = false;}else {popup.classList.remove("popup-down");close = true;}});
--- js/script.js
+++ js/script.js
@@ -2,16 +2,17 @@ var link = document.querySelector(".search-btn");
 var popup = document.querySelector(".search-form");
 var close = true;
 
-	link.addEventListener("click", function(event) {
+link.addEventListener("click", function(event) {
+	
 	event.preventDefault();
-		
-		if (close) {
-			popup.classList.add("popup-down");
-			close = false;
-		}
-		
-		else {
-			popup.classList.remove("popup-down");
-			close = true;
-		}
-	});													
+
+	if (close) {
+		popup.classList.add("popup-down");
+		close = false;
+	}
+
+	else {
+		popup.classList.remove("popup-down");
+		close = true;
+	}
+});													
